bc39363ddea8277d505212e78196a3762d2cbf43
Ryo Murakami <integral14@gmail.com>
2020-05-09 16:57:39 +0900

    github actions (#2)
    * github actions
    
    * on push
    
    * on push
    
    * on push
    
    * on push
    
    * on push
    
    * on push
    
    * on push
    
    * on push
    
    * on push
    
    * on push

---
+++ update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -eu
+
+echo "configuring git"
+git config --global user.name "xeno14"
+git config --global user.email "integral14.dev@gmail.com"
+git remote set-url origin https://xeno14:${GITHUB_TOKEN}@github.com/xeno14/gitignore_global_concat
+git checkout master
+
+echo "updating submodule"
+git submodule update --init
+git submodule foreach git fetch
+cd gitignore
+git checkout master
+git rebase origin/master
+GITIGNORE_HASH=$(git rev-parse --short HEAD)
+cd ..
+
+echo "generating gitignore_global"
+bash concat.sh
+
+if [ -z "$(git diff .gitignore_global)" ]; then
+    # Working directory clean
+    echo "Working directory is clean. No need to commit."
+  else
+    # Uncommitted changes
+    echo "Detected upstream changes. The latest commit=${GITIGNORE_HASH}."
+
+    git commit -a -m"[GitHub Actions] ${GITIGNORE_HASH}"
+    git push origin HEAD
+
+    echo "Done."
+fi
